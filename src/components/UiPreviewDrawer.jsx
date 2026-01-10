7e7d99b26f7af1882f16a5c4af01c637ef1c5ce5
TravelTable <jackt1263@gmail.com>
2026-01-10 18:59:53 +1100

    dsadsdsd

--- src/components/UiPreviewDrawer.jsx
+++ src/components/UiPreviewDrawer.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState, useEffect, useCallback } from "react";
-import { X, Download, Copy, Check, Loader, Search, Image as ImageIcon, AlertCircle, ExternalLink } from "lucide-react";
+import { X, Download, Copy, Check, Loader, Search, Image as ImageIcon, AlertCircle, ExternalLink, Plus } from "lucide-react";
 import LuaPreviewRenderer from "../preview/LuaPreviewRenderer";
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import luaLang from "react-syntax-highlighter/dist/esm/languages/hljs/lua";
